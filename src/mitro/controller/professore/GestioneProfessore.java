d5f3218554ad7e2c4809e9a2f6319bdd4e111047
federico <federico@DESKTOP-JBCNVR8>
2019-05-21 10:26:55 +0200

    Aggiunti i metodi all'interfaccia "GestioneProfessore"

--- src/mitro/controller/professore/GestioneProfessore.java
+++ src/mitro/controller/professore/GestioneProfessore.java
@@ -1,5 +1,19 @@
 package mitro.controller.professore;
 
-public interface GestioneProfessore {
+import java.time.LocalDate;
+import java.util.List;
 
+import mitro.exceptions.OperazioneException;
+import mitro.model.Attivita;
+import mitro.model.Classe;
+import mitro.model.Comunicazione;
+import mitro.model.Professore;
+
+public interface GestioneProfessore {
+	
+	public Professore getProfessore() throws OperazioneException;
+	public List<Classe> getListaClassi() throws OperazioneException;
+	public List<Attivita> getListaAttivita(LocalDate from, LocalDate to) throws OperazioneException;
+	public List<Comunicazione> getListaComunicazioni(LocalDate from, LocalDate to) throws OperazioneException;
+	
 }
--- src/mitro/controller/professore/GestioneProfessoreController.java
+++ src/mitro/controller/professore/GestioneProfessoreController.java
@@ -1,5 +1,38 @@
 package mitro.controller.professore;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import mitro.exceptions.OperazioneException;
+import mitro.model.Attivita;
+import mitro.model.Classe;
+import mitro.model.Comunicazione;
+import mitro.model.Professore;
+
 public class GestioneProfessoreController implements GestioneProfessore {
+
+	@Override
+	public Professore getProfessore() throws OperazioneException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Classe> getListaClassi() throws OperazioneException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Attivita> getListaAttivita(LocalDate from, LocalDate to) throws OperazioneException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Comunicazione> getListaComunicazioni(LocalDate from, LocalDate to) throws OperazioneException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
