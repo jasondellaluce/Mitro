5199a7cdf8e2f0b2a9a2f730d68f7ae5462209f7
federico <federico@DESKTOP-JBCNVR8>
2019-05-21 12:37:16 +0200

    Aggiunte classi di test per Professore e Classe

---
+++ test/mitro/controller/professore/GestioneClasseControllerTest.java
@@ -0,0 +1,46 @@
+package mitro.controller.professore;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import mitro.controller.log.LoggerOperazioni;
+import mitro.controller.log.MockLoggerOperazioni;
+import mitro.exceptions.OperazioneException;
+import mitro.model.Classe;
+import mitro.persistenza.DAOArchiviazione;
+import mitro.persistenza.DAOAttivita;
+import mitro.persistenza.DAOUtente;
+import mitro.persistenza.collection.CollectionDAOArchiviazione;
+import mitro.persistenza.collection.CollectionDAOAttivita;
+import mitro.persistenza.collection.CollectionDAOUtente;
+
+public class GestioneClasseControllerTest {
+
+	private static GestioneClasseController test;
+	private static Classe classe;
+	private static DAOArchiviazione daoArchiviazione;
+	private static DAOUtente daoUtente;
+	private static DAOAttivita daoAttivita;
+	private static LoggerOperazioni logger;
+	
+	@BeforeAll
+	static void inizializza() {
+		classe = new Classe();
+		classe.setId("TestID");
+		classe.setNome("Amir");
+		daoArchiviazione = new CollectionDAOArchiviazione();
+		daoUtente = new CollectionDAOUtente();
+		daoAttivita = new CollectionDAOAttivita();
+		logger = new MockLoggerOperazioni();
+		test = new GestioneClasseController(daoArchiviazione, daoUtente,
+				daoAttivita, logger);
+	}
+	
+	@Test
+	void testGetClasse() throws OperazioneException {
+		assertEquals(test.getClasse(), classe);
+	}
+	
+}
---
+++ test/mitro/controller/professore/GestioneProfessoreControllerTest.java
@@ -0,0 +1,42 @@
+package mitro.controller.professore;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import mitro.controller.log.LoggerOperazioni;
+import mitro.controller.log.MockLoggerOperazioni;
+import mitro.exceptions.OperazioneException;
+import mitro.model.Professore;
+import mitro.persistenza.DAOAttivita;
+import mitro.persistenza.DAOComunicazione;
+import mitro.persistenza.collection.CollectionDAOAttivita;
+import mitro.persistenza.collection.CollectionDAOComunicazione;
+
+public class GestioneProfessoreControllerTest {
+
+	private static GestioneProfessoreController test;
+	private static Professore professore;
+	private static DAOComunicazione daoComunicazione;
+	private static DAOAttivita daoAttivita;
+	private static LoggerOperazioni logger;
+	
+	@BeforeAll
+	static void inizializza() {
+		professore = new Professore();
+		professore.setId("TestID");
+		professore.setNome("Federico");
+		daoComunicazione = new CollectionDAOComunicazione();
+		daoAttivita = new CollectionDAOAttivita();
+		logger = new MockLoggerOperazioni();
+		test = new GestioneProfessoreController(professore, daoComunicazione,
+				daoAttivita, logger);
+	}
+	
+	@Test
+	void testGetProfessore() throws OperazioneException {
+		assertEquals(test.getProfessore(), professore);
+	}
+	
+}
