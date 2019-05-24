154876b2ba48ebd402a7e0fa2fa239f1646dc9b1
federico <federico@DESKTOP-JBCNVR8>
2019-05-24 10:59:48 +0200

    Default Html Professore HomePage

--- test/mitro/controller/studente/GestioneStudenteControllerTest.java
+++ test/mitro/controller/studente/GestioneStudenteControllerTest.java
@@ -53,22 +53,27 @@ class GestioneStudenteControllerTest {
 
 	@Test
 	void testGetListaAttivita() throws OperazioneException {
+		
 		Professore professoreStoria = new Professore("Federico", "Baldini",
 				"FB@mail.it", "Via A 1", "333000111222", LocalDate.now());
 		Professore professoreMatematica = new Professore("Amir", "Al Sadi",
 				"AS@mail.it", "Via B 2", "333111222333", LocalDate.now());
+		
 		Materia materiaStoria = new Materia("001", "storia",
 				"storia informatica");
 		Materia materiaMatematica = new Materia("002", "matematica",
 				"matematica chimica");
+		
 		Classe classeA = new Classe("001", "classe A", "2019",
 				"3° informatica");
 		Classe classeB = new Classe("002", "classe B", "2019",
 				"4° chimica");
+		
 		Attivita lezioneStoria = new Attivita(professoreStoria,
 				LocalDate.of(2019, 1, 2), 9, materiaStoria, "lezione di storia", classeA);
 		Attivita lezioneMatematica = new Attivita(professoreMatematica,
 				LocalDate.of(2019, 2, 3), 10, materiaMatematica, "lezione di matematica", classeB);
+		
 		try {
 			daoAttivita.registraAttivita(lezioneStoria);
 			daoAttivita.registraAttivita(lezioneMatematica);
