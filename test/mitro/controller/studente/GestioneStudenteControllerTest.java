040bb5bd4f58f93c51e10fbc427ace18666678a2
Jason Dellaluce <jason.dellaluce@studio.unibo.it>
2019-05-24 11:14:06 +0200

    Fix merge

--- test/mitro/controller/studente/GestioneStudenteControllerTest.java
+++ test/mitro/controller/studente/GestioneStudenteControllerTest.java
@@ -45,42 +45,7 @@ class GestioneStudenteControllerTest {
 
 	@Test
 	void testGetListaAttivita() throws OperazioneException {
-<<<<<<< HEAD
 		// TODO: Implement test
-=======
-		
-		Professore professoreStoria = new Professore("Federico", "Baldini",
-				"FB@mail.it", "Via A 1", "333000111222", LocalDate.now());
-		Professore professoreMatematica = new Professore("Amir", "Al Sadi",
-				"AS@mail.it", "Via B 2", "333111222333", LocalDate.now());
-		
-		Materia materiaStoria = new Materia("001", "storia",
-				"storia informatica");
-		Materia materiaMatematica = new Materia("002", "matematica",
-				"matematica chimica");
-		
-		Classe classeA = new Classe("001", "classe A", "2019",
-				"3° informatica");
-		Classe classeB = new Classe("002", "classe B", "2019",
-				"4° chimica");
-		
-		Attivita lezioneStoria = new Attivita(professoreStoria,
-				LocalDate.of(2019, 1, 2), 9, materiaStoria, "lezione di storia", classeA);
-		Attivita lezioneMatematica = new Attivita(professoreMatematica,
-				LocalDate.of(2019, 2, 3), 10, materiaMatematica, "lezione di matematica", classeB);
-		
-		try {
-			daoAttivita.registraAttivita(lezioneStoria);
-			daoAttivita.registraAttivita(lezioneMatematica);
-			List<Attivita> listaAttivita = test.getListaAttivita(LocalDate.of(2018, 1, 1), LocalDate.of(2020, 1, 1));
-			//la lista è vuota
-			assertEquals(listaAttivita.get(0), lezioneStoria);
-			assertEquals(listaAttivita.get(1), lezioneMatematica);
-		}
-		catch (PersistenzaException e) {
-			throw new OperazioneException(e);
-		}
->>>>>>> branch 'master' of https://gitlab.com/jasondellaluce/mitro.git
 	}
 
 	@Test
