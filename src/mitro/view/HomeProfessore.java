fca4834b277bee62d343083955c0dfef2e06dbd8
federico <federico@DESKTOP-JBCNVR8>
2019-05-24 12:36:51 +0200

    Add default Getter "HomeProfessore "

--- src/mitro/view/HomeProfessore.java
+++ src/mitro/view/HomeProfessore.java
@@ -1,13 +1,20 @@
 package mitro.view;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import mitro.controller.log.LoggerMessaggi;
+import mitro.model.Attivita;
+import mitro.model.Materia;
+import mitro.model.Presenza;
 import mitro.model.Ruolo;
 import mitro.model.Utente;
+import mitro.model.Voto;
 
 public class HomeProfessore extends ViewUtenteAstratta {
 
@@ -15,6 +22,8 @@ public class HomeProfessore extends ViewUtenteAstratta {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private LoggerMessaggi logger;
 
 	public HomeProfessore(Ruolo ruoloPrivilegio) {
 		super(ruoloPrivilegio);
@@ -33,6 +42,22 @@ public class HomeProfessore extends ViewUtenteAstratta {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public List<Materia> getListaMaterie() {
+		return null;
+	}
+	
+	public List<Voto> getListaVoti() {
+		return null;
+	}
+	
+	public List<Presenza> getListaPresenza() {
+		return null;
+	}
+	
+	public List<Attivita> getListaAttivita(LocalDate from, LocalDate to) {
+		return null;
 	}
 
 }
